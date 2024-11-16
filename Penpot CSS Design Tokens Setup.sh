83dc6ef0582e603ba92027b4f494f754340628bf
RRR <41332950+Raphael-Regnier@users.noreply.github.com>
2024-11-16 13:53:46 +0100

    Delete Penpot CSS Design Tokens Setup.sh

--- Penpot CSS Design Tokens Setup.sh
+++
@@ -1,9 +0,0 @@
-# Create a new directory and navigate into it
-mkdir design-tokens-plugin
-cd design-tokens-plugin
-
-# Initialize npm project
-npm init -y
-
-# Install necessary dependencies
-npm install --save-dev vite typescript @vitejs/plugin-react @penpot/plugin-types @penpot/plugin-styles react react-dom @types/react @types/react-dom
